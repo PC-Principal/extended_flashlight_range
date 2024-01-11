1477b5b3f205789e8673abc180b76d65d9cc94df
Vasiliy <basil2305@ya.ru>
2024-01-11 18:00:59 +0300

    Initial commit

---
+++ LethalCompanyTemplate/Plugin.cs
@@ -0,0 +1,16 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+
+namespace LethalCompanyTemplate
+{
+    [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+    public class Plugin : BaseUnityPlugin
+    {
+        private void Awake()
+        {
+            // Plugin startup logic
+            Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,8 @@
+# LethalCompanyTemplate
+A template for a BepInEx 5 Plugin, using the correct .csproj configuration for Lethal Company modding
+
+
+Клонирован отсюда: https://github.com/LethalCompany/LethalCompanyTemplate
+Больше инфы о том что с этим делать: https://www.youtube.com/watch?v=4Q7Zp5K2ywI&t=1092s
+
+Этот репозиторий является базовым шаблоном для создания модов, его можно использовать - как точку начала
