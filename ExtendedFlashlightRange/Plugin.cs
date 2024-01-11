e4fe43fb180360a504cdc10c348f7ab103e46e2f
PC_Principal <basil2305@ya.ru>
2024-01-11 21:27:16 +0300

    Setup Project Core

--- ExtendedFlashlightRange/Plugin.cs
+++ ExtendedFlashlightRange/Plugin.cs
@@ -1,16 +1,54 @@
-﻿using BepInEx;
+﻿using System;
+using BepInEx;
 using BepInEx.Logging;
 using HarmonyLib;
+using MonoMod.ModInterop;
 
+/* Mod namespace */
 namespace ExtendedFlashlightRange
 {
-    [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
-    public class Plugin : BaseUnityPlugin
+    /* required params for BepInEx Plugin */
+    [BepInPlugin(modGUID, modName, modVersion)]
+
+    /* Class with mod logic */
+    public class ExtendedFlashlightRangeModBase : BaseUnityPlugin
     {
-        private void Awake()
+        /* Mod unique name */
+        private const string modGUID = "PC_Principal.ExtendedFlashlightRangeMod";
+
+        /* Mode name in store */
+        private const string modName = "Extended Flashlight Range Mod";
+
+        /* mod version */
+        private const string modVersion = "1.0.0";
+
+        /** readonly variable with harmony lib for mod */
+        private readonly Harmony harmony = new Harmony(modGUID);
+
+        /** Instance of current class */
+        private static ExtendedFlashlightRangeModBase Instance;
+
+        /* Internal log journal */
+        internal ManualLogSource log_journal;
+        
+        /* Called when plugin initiated */
+        void Awake()
         {
-            // Plugin startup logic
-            Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
+            /* Check if instance if not exists */
+            if (Instance == null)
+            {
+                /* Set instance like this class */
+                Instance = this;
+            }
+
+            /* Log object for current mod */
+            log_journal = BepInEx.Logging.Logger.CreateLogSource(modGUID);
+            
+            /* String of mod Initiated (Modname and its version) */
+            log_journal.LogInfo($"{modName} initiated: v{modName}");
+            
+            /* Patch all with type of current class */
+            harmony.PatchAll(typeof(ExtendedFlashlightRangeModBase));
         }
     }
 }
